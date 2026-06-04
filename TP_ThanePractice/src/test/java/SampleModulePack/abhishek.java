60bea3df10ca6a1b819233d404e04aac2b24e287
abhiwaykar-svg <abhiwaykar@gmail.com>
2026-06-04 12:36:49 +0530

    abh

---
+++ TP_ThanePractice/src/test/java/SampleModulePack/abhishek.java
@@ -0,0 +1,11 @@
+package SampleModulePack;
+
+public class abhishek {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("abhishek");
+	}
+
+}
