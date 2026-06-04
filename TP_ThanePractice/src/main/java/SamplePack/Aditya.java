ffdd5232e2c7ee32cedaa34f409302db4c81e774
aadikhare <aadi.khare88@gmail.com>
2026-06-04 12:56:56 +0530

    this is second commit

--- TP_ThanePractice/src/main/java/SamplePack/Aditya.java
+++ TP_ThanePractice/src/main/java/SamplePack/Aditya.java
@@ -4,7 +4,9 @@ public class Aditya {
 
 	public static void main(String[] args) {
 	
-		System.out.println("thi is first code");
+		System.out.println("this is first code");
+		System.out.println("this is second code");
+		
 
 	}
 
