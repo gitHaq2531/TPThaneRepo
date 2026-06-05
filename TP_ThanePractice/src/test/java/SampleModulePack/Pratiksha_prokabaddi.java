81d6b81fdcaa2ac1d12267ab4c4b797615402118
Pratiksha1501199 <122699860+Pratiksha1501199@users.noreply.github.com>
2026-06-05 17:15:15 +0530

    c2

--- TP_ThanePractice/src/test/java/SampleModulePack/Pratiksha_prokabaddi.java
+++ TP_ThanePractice/src/test/java/SampleModulePack/Pratiksha_prokabaddi.java
@@ -1,5 +1,37 @@
 package SampleModulePack;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Pratiksha_prokabaddi {
+	
+	public static void main(String[] args) {
+		
+		
+		  WebDriver driver = new ChromeDriver();
+	        driver.manage().window().maximize();
+
+	        driver.get("https://www.prokabaddi.com");
+	        
+	       driver.findElement(By.xpath("//span[text()='Standings']")).click();
+	       
+	       List<WebElement> wins =driver.findElements(By.xpath("//ul[@class=\"form-listing\" and contains(.,'WWW')]/parent::div[@class=\"table-data form\"]/preceding-sibling::div[@class='table-data team']"));
+	     
+	       for(WebElement row : wins) {
+	    	   String s=row.getText();
+	    	   System.out.println(s);
+	    	   
+	       }
+	       
+	       
+	       
+	}
 
 }
+
+
+
