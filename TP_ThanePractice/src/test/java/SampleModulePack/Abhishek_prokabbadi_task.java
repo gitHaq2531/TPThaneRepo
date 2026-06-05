77245f8edfd981e0800dd52857c9d6bb86fb9756
abhiwaykar-svg <abhiwaykar@gmail.com>
2026-06-05 17:09:19 +0530

    new file

---
+++ TP_ThanePractice/src/test/java/SampleModulePack/Abhishek_prokabbadi_task.java
@@ -0,0 +1,34 @@
+package SampleModulePack;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Abhishek_prokabbadi_task {
+	
+	@Test
+	public void porkabbadiyteam() {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.prokabaddi.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+		driver.findElement(By.xpath("//a[@title=\"Standings\"]")).click();
+
+		List<WebElement> ele = driver.findElements(By.xpath(
+				"//ul[@class=\"form-listing\" and contains(.,'WWW')]/parent::div[@class=\"table-data form\"]/preceding-sibling::div[@class=\"table-data team\"]"));
+
+		for (WebElement name : ele) {
+
+			String st = name.getText();
+			System.out.println(st);
+		}
+	
+	}
+}
