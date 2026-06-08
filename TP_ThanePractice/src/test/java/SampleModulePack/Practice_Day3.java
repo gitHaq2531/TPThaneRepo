d71b8e20ac400675fdb5b41af83775beb2e322f2
kanchanv1016-aks <kanchanv1016@gmail.com>
2026-06-08 10:54:17 +0530

    New Class added

---
+++ TP_ThanePractice/src/test/java/SampleModulePack/Practice_Day3.java
@@ -0,0 +1,15 @@
+package SampleModulePack;
+
+import org.testng.annotations.Test;
+
+public class Practice_Day3 {
+	
+	@Test 
+	
+public void practice() {
+		
+		System.out.println("New Class Created");
+		
+	}
+
+}
